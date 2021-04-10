7e57a8c4b9bf3a0740279839357e444eb5fcec37
sijan joshi <joshisijan96@gmail.com>
2021-04-10 09:01:02 +0545

    solved networkHelper conflict

--- lib/helpers/networkhelper.dart
+++ lib/helpers/networkhelper.dart
@@ -1,29 +1,28 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class NetworkHelper{
-  NetworkHelper({this.startLng,this.startLat,this.endLng,this.endLat});
+class NetworkHelper {
+  NetworkHelper({this.startLng, this.startLat, this.endLng, this.endLat});
 
-
-
-  final String url ='https://api.openrouteservice.org/v2/directions/';
-  final String apiKey = '5b3ce3597851110001cf62483cf045ebf5dc4cee9f4062e813667c91';
-  final String journeyMode = 'driving-car'; // Change it if you want or make it variable
+  final String url = 'https://api.openrouteservice.org/v2/directions/';
+  final String apiKey =
+      '5b3ce3597851110001cf62483cf045ebf5dc4cee9f4062e813667c91';
+  final String journeyMode =
+      'driving-car'; // Change it if you want or make it variable
   final double startLng;
   final double startLat;
   final double endLng;
   final double endLat;
 
-  Future getData() async{
-    http.Response response = await http.get('$url$journeyMode?api_key=$apiKey&start=$startLng,$startLat&end=$endLng,$endLat');
-    print("$url$journeyMode?$apiKey&start=$startLng,$startLat&end=$endLng,$endLat");
+  Future getData() async {
+    String stringUrl =
+        '$url$journeyMode?api_key=$apiKey&start=$startLng,$startLat&end=$endLng,$endLat';
+    http.Response response = await http.get(Uri.parse(stringUrl));
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       String data = response.body;
       return jsonDecode(data);
-
-    }
-    else{
+    } else {
       print(response.statusCode);
     }
   }
